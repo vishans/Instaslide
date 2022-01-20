d272e3e3b1eca9eac13cadffffe05d47e429597c
Vishan Summinga Sonagadu <94801273+vishans@users.noreply.github.com>
2022-01-21 02:54:22 +0400

    Initial commit

--- src/post.py
+++ src/post.py
@@ -186,64 +186,7 @@ class Post:
 # creating the post
 p = Post()
 
-# adding front cover
-title = '<#494F55>Ban aktialité</#494F55>'
-sub_title = '<#494F55>20&/01</#494F55>'
-p.addFrontCover(frameColor='#7C7C7C', title = title, sub_title=sub_title, swipeIconColor=(73, 79, 85))
 
-# p.addFrontCover()
-#
-
-article = '''Plizier lendroit dans <red>M</red><blue>or</blue><gold>i</gold><green>s</green> ti <blue>innondé</blue> hier akoz tiena <blue>gro lapli</blue>. <blue>Piton</blue> ou Pitan comme dirait arjoon, ti emba délo. La route ti innondé - délo ti p fini rent dans bus tout.
-Dan le sud si ban place couma <blue>La Flora</blue> ek <blue>Rose Belle</blue> inn bien gagné.
-Bnla p dire tiena <#3944BC>vague</#3944BC> tout dans <#3944BC>lopital Rose Belle</#3944BC>.
-<gray>Vidéo lor 2 prochain page</gray>'''
-
-p.addSlide(article=article, flairColor='#1338BE', flairText='Pié dan lo')
-
-
-p.skipAPage()
-p.skipAPage()
-
-#
-
-article ='''Encore dans thème gros lapli meme. <red>Madagascar</red> finn experience ban <orangered>la pli torrentiel</orangered> ces derniers zours. Dan capital, Antananarivo, plis ki <orangered>500</orangered> dimoune in <orangered>perdi zot lakaz</orangered>. 
-Ek <red>10 dimoune in mort</red>. La plipart ban victime la in périr dans <orangered>glissement terrain</orangered>. Ban Malgache p attan zot à encore plis lapli.
-Ban expert p dir sa ban gros lapli la p arrivé akoz <red>dérèglement climatique</red>. '''
-
-p.addSlide(article=article, flairColor='grey',flairText='madagascar')
-
-#
-
-article = '''Ena enn rézo <hotpink>prostitution</hotpink> ki p deroule dans l'ouest. Dapre lord cervo operation la enn <orangered>couple franco-morisien</orangered>. Banla amen fam dpi la <red>Russie</red> ek <red>Ukraine</red> pou chocho. Selmn ban fam la <red>pa rod chocho avec morisien</red> zot rod zis <red>etranzé</red>. Cervo la ti dir enn des ban prostitué la, Liudmila, dormi avc enn morisien. Fam la pann dakor. Lerlaem caca in alle fanné. Liudmila in alle dévwal zot secret la police. By the way, enn la nuit coute <red>Rs 130 000</red>.'''
-
-p.addSlide(article=article, flairText='seks', flairColor='hotpink')
-
-article = '''Tiena enn <red>eruption volcanique</red> dan <red>Tongatapu</red>, enn <orangered>l'île</orangered> dan l'océan Pacfique. Sa inn déclanse enn <blue>tsunami</blue>. Omoin <red>3 dimoun in mort</red> ek <orangered>150 lakaz in endomazé</orangered>. L'Australie, la France ek Nouvelle Zelande p rod avoy laide, mais avion pas p kav atterrir akoz ena enta lasann volcanique.
-<gray>Tongatapu cest l'île principal Tonga, enn group l'île dans l'océan Pacifique. Tongatapu </gray><red>meme</red><gray> grandeur ar district</gray> <orangered>Rivière Noire</orangered>.'''
-
-p.addSlide(article=article, flairText='tongatapu')
-
-
-article = '''L'<red>Allemagne</red> ek <red>Brésil</red> finn enregistré enn <red>record</red> nombre cas <crimson>covid</crimson> <red>sans précédent</red> en 1 zour.
-<red>Australie</red> so main dev in paralysé akoz virus la. Ban expert p dire cest <red>Omicron</red> ki p kass fess coumsa la. <red>Japon</red> so gouvernment inn dir li pou récoumense met restriction en place. La <red>Chine</red> inn arrete vende ticket pou <blue>Jeux olympiques d'hiver</blue> akoz covid. <gray>JO d'hiver sipozé ena lieu à Pékin (Beijing) le 4 ziska le 20 février 2022.</gray> '''
-
-p.addSlide(article=article, flairText='covid-19',flairColor='#99241C')
-
-
-
-
-article ='''<orangered>Microsoft</orangered> inn asté <orangered>Activision Blizzard</orangered> pou <red>$69 milliard</red>. 
-Activision meme ki develop ban zoué kouma <orangered>Call of Duty</orangered>, <orangered>Candy Crush</orangered> ek <orangered>World of Warcraft</orangered>. Microsoft pou met sa ban zoué dans so Xbox Game Pass.'''
-
-
-p.addSlide(article=article, flairText='tech',flairColor='silver')
-
-# p.addFrontCover()
-
-
-
-p.compile(fList=['.jpg']*2 + ['.jfif']+['.jpg']*2 + ['.jpeg', '.jpg'])
 
 
 
