25562a5de46419f51ac5c33fbbe2f0c8e2c2e5b5
Vishan Summinga Sonagadu <94801273+vishans@users.noreply.github.com>
2022-01-21 15:29:44 +0400

    Grammatical mistakes

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from post import Post
 
 '''Always use the  post API to generate slides or story slides.
 Do NOT use CorePostCreator. 
-Post is a wrapper around CorePostCreator to abstract and simplify it's operation.
-Use CorePostCreator only if you need to modify core parameter not possible through Post.
+Post is a wrapper around CorePostCreator to abstract and simplify its operation.
+Use CorePostCreator only if you need to modify core parameters not possible through Post.
 Beware that CorePostCreator was not designed to be used by the user.'''
